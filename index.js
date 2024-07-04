b967e0b81ec68f4fc5eb5a7cce5118b9a5852620
CARLOS CESAR JUNIOR DOS SANTOS <carlos_cj_santos@estudante.sesisenai.org.br>
2024-07-04 17:35:39 -0300

    Atualização

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require('express')
+const {Client} = require('pg')
+require("dotenv").config()
+
+const client = new Client({
+    host:process.env.host,
+    port:process.env.port,
+    user:process.env.user,
+    password:process.env.password,
+    database:process.env.database
+})
 
 const app = express()
+
+const connectDB = async () => {
+    client
+    .connect()
+    .then(() => {
+        console.log(' a conexão funcionou ')
+    }).catch((err) => {
+        console.log('Erro ao conectar')
+    });
+};
+
+connectDB()
 
 app.get('/', function(req, res){
     res.send(' QUEM LEU É GAY ')
